e6deb6bf68dc773230b07d7158ff8e219a02ce45
kerrieyee <kerrie@kerrieyee.com>
2013-01-13 18:21:31 -0800

    created users model with specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  validates :name, :password, presence: true
+  validates :email, presence: true, uniqueness: true
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+
+  has_many :job_prospects
   # attr_accessible :title, :body
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+	subject{Fabricate(:user)}
+
+  [:name, :password, :email].each do |field|
+  	it {should validate_presence_of field}
+  end
+
+  it {should validate_uniqueness_of :email}
+  it {should have_many :job_prospects}
 end
