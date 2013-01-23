d0e4b5ebae064df0176335e0c71ec40857657985
kerrieyee <kerrie@kerrieyee.com>
2013-01-22 20:26:39 -0800

    Adding feature that will allow you to add and edit job_prospect in the same page
    mostly just JS, views, and some controller additions for job_prospects

--- app/controllers/job_prospects_controller.rb
+++ app/controllers/job_prospects_controller.rb
@@ -1,13 +1,23 @@
 class JobProspectsController < ApplicationController
 	def index
 		@job_prospects = JobProspect.where(:user_id => current_user.id)
+		@job_prospect=JobProspect.new
 	end
+
+	def new
+		@job_prospect=JobProspect.new
+		respond_to do |format| 
+			format.html { render :action => "new" }
+			format.js
+		end
+	end
+
 
 	def create
 		@job_prospects = JobProspect.where(:user_id => current_user.id)
 		@job_prospect = JobProspect.new(	:company => params[:job_prospect][:company],
-																				:position => params[:job_prospect][:position],
-																				:user => current_user)
+																			:position => params[:job_prospect][:position],
+																			:user => current_user)
 		respond_to do |format|
 			if @job_prospect.save
 				format.html{redirect_to job_prospects_url}
@@ -16,6 +26,25 @@ class JobProspectsController < ApplicationController
 				format.html{ render :action => "create"}
 				format.js
      end			
+		end
+	end
+
+	def edit
+		@job_prospect = JobProspect.find(params[:id])
+		respond_to { |format| format.js }
+	end 
+
+	def update
+		@job_prospect = JobProspect.find(params[:id])
+
+		respond_to do |format|
+			if @job_prospect.update_attributes(params[:job_prospect])
+				format.html{redirect_to job_prospects_url}
+				format.js
+			else
+				format.html{ render :action => "edit"}
+				format.js
+     	end			
 		end
 	end
 
