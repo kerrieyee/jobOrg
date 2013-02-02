100f5f5087e992a1c4b7656d74130e632b0b13f5
kerrieyee <kerrie@kerrieyee.com>
2013-02-01 17:39:19 -0800

    made controller changes to jobProspects controller

--- app/controllers/job_prospects_controller.rb
+++ app/controllers/job_prospects_controller.rb
@@ -1,60 +1,45 @@
 class JobProspectsController < ApplicationController
+	respond_to :json
 	def index
 		@job_prospects = JobProspect.paginate(:page => params[:page],
                            :per_page   => 8,
                            :order      => 'created_at DESC',
                            :conditions => { :user_id => current_user.id })
-		@job_prospect=JobProspect.new
+		respond_with (@job_prospects)
 	end
 
-	def new
-		@job_prospect=JobProspect.new
-		respond_to do |format| 
-			format.html { render :action => "new" }
-			format.js
-		end
-	end
+	# def new
+	# 	@job_prospect=JobProspect.new
+	# 	respond_to do |format| 
+	# 		format.html { render :action => "new" }
+	# 		format.js
+	# 	end
+	# end
 
 
 	def create
-		@job_prospects = JobProspect.where(:user_id => current_user.id)
 		@job_prospect = JobProspect.new(	:company => params[:job_prospect][:company],
 																			:position => params[:job_prospect][:position],
 																			:user => current_user)
-		respond_to do |format|
-			if @job_prospect.save
-				format.html{redirect_to job_prospects_url}
-				format.js
-			else
-				format.html{ render :action => "create"}
-				format.js
-     end			
-		end
+		@job_prospect.save ? respond_with(@job_prospect) : respond_with(@job_prospect, :status => unprocessable_entity)
 	end
 
-	def edit
-		@job_prospect = JobProspect.find(params[:id])
-		respond_to { |format| format.js }
-	end 
+	# def edit
+	# 	@job_prospect = JobProspect.find(params[:id])
+	# 	respond_to { |format| format.js }
+	# end 
 
 	def update
 		@job_prospect = JobProspect.find(params[:id])
 
-		respond_to do |format|
-			if @job_prospect.update_attributes(params[:job_prospect])
-				format.html{redirect_to job_prospects_url}
-				format.js
-			else
-				format.html{ render :action => "edit"}
-				format.js
-     	end			
-		end
+		@job_prospect.update_attributes(params[:job_prospect]) ? respond_with(@job_prospect) : respond_with(@job_prospect, :status => unprocessable_entity)
 	end
 
 	def show
 		@job_prospect = JobProspect.find_by_id_and_user_id(params[:id], current_user.id)
 		if @job_prospect
 			@events = Event.where(:job_prospect_id => @job_prospect.id).order("conversation_date").limit(5)
+			respond_with(@job_prospect, @events)
 		else
 			redirect_unauthorized_user
 		end
@@ -62,11 +47,7 @@ class JobProspectsController < ApplicationController
 
 	def destroy
 		@job_prospect = JobProspect.find(params[:id])
-		@job_prospect.destroy
-		respond_to do |format|
-			format.html{redirect_to job_prospects_url}
-			format.js
-		end
+		respond_with(@job_prospect.destroy)
 	end
 
 end
