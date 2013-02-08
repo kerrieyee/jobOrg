0bcdad3afdac3ca566ab9b7eb57ffd367f8e8f41
kerrieyee <kerrie@kerrieyee.com>
2013-02-08 00:50:38 -0800

    refactored events controller and wrote new specs

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,7 +11,6 @@ class EventsController < ApplicationController
                            :per_page   => 8,
                            :order      => 'conversation_date DESC',
                            :conditions => { :job_prospect_id => @job_prospect.id })
-			#@events = Event.where(:job_prospect_id => @job_prospect.id).order("conversation_date")
 		else
 			redirect_unauthorized_user
 		end
@@ -25,13 +24,12 @@ class EventsController < ApplicationController
 	def create
 		job_prospect = {:job_prospect => JobProspect.find(params[:job_prospect_id])}
 		@event = Event.new(params[:event].merge(job_prospect))
-		@event.update_job_prospect
 		
 		if @event.save
 			flash[:success] = "Your event has been saved."
 			redirect_to job_prospect_events_path
 		else
-			flash.now[:error] = "Invalid event.  #{display_errors(@event)}"
+			flash.now[:error] = "Invalid event.  #{@event.display_errors}."
 			render "new"
 		end
 	end
@@ -49,13 +47,12 @@ class EventsController < ApplicationController
 	def update
 		@event = Event.find(params[:id])
 		job_prospect = {:job_prospect => @event.job_prospect}
-		@event.update_job_prospect
 		
 	  if @event.update_attributes(params[:event].merge(job_prospect))
 	  	flash[:success] = "Your event has been successfully updated!"
 	   	redirect_to job_prospect_events_path(@event.job_prospect)
 	 	else
-	 		flash.now[:error] = "Invalid event.  #{display_errors(@event)}"
+	 		flash.now[:error] = "Invalid event.  #{@event.display_errors}."
 	 		render "edit"
     end
   end
@@ -64,6 +61,7 @@ class EventsController < ApplicationController
 		@event = Event.find(params[:id])
 		job_prospect = @event.job_prospect
 		@event.destroy
+
 		respond_to do |format|
 			format.html{redirect_to job_prospect_events_path(job_prospect)}
 			format.js
@@ -71,18 +69,13 @@ class EventsController < ApplicationController
 	end
 
 	def all_events
-		#need to fix very inefficient
-		@collection_events = []
-		Event.all.each{ |event|  @collection_events<<event if correct_user?(event.job_prospect.user, current_user) }
-		@events = @collection_events.paginate(:page       => params[:page],
-                           :per_page   => 8,
-                           :order      => 'conversation_date DESC')
+		@events = Event.all_events(current_user)
+		@page_events = @events.paginate(	:page       => params[:page],
+                           						:per_page   => 8,
+                           						:order      => 'conversation_date DESC')
 	end
 
 	private
-	def display_errors(object)
-		object.errors.full_messages.join(". ")
-	end
 
 	def authorize_user(event)
 		correct_user?(event.job_prospect.user, current_user) ? @event = event : redirect_unauthorized_user
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,10 +5,24 @@ class Event < ActiveRecord::Base
   belongs_to :job_prospect
   has_many :documents, :dependent => :destroy
   accepts_nested_attributes_for :documents
+  
+  before_create :update_job_prospect_last_updated  
+  before_update :update_job_prospect_last_updated 
 
-
-  def update_job_prospect
+  def update_job_prospect_last_updated
   	self.job_prospect.last_updated = Time.now
   	self.job_prospect.save
   end
+
+  def display_errors
+    self.errors.full_messages.join(". ")
+  end
+
+  def self.all_events(actual_user)
+    events = []
+    self.all.each {|e| events << e if e.job_prospect.user == actual_user}
+    events
+  end
+
+
 end
